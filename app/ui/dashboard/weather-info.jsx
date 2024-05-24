f9a911a328e703eb0d1d8971a15fc95143b40a28
Grexgi <rfachri.exe@gmail.com>
2024-05-24 09:56:47 +0700

    mengubah layout weather-info

--- app/ui/dashboard/weather-info.jsx
+++ app/ui/dashboard/weather-info.jsx
@@ -12,7 +12,7 @@ export default function WeatherInfo() {
       const apiUrl = `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}&language=id-id`;
       const response = await fetch(apiUrl);
       const data = await response.json();
-      console.log(data)
+      
       setWeatherData(data[0]); // Assuming the first element in the response contains the current conditions
     };
 
@@ -24,28 +24,31 @@ export default function WeatherInfo() {
   }
 
   return (
-    <div className="flex flex-row flex-wrap gap-1 p-4 md:flex-row">
+    <div className="flex flex-row flex-wrap gap-1 p-4">
       <h1 className="w-full text-2xl font-bold">Cuaca</h1>
-      <p id="date" className="w-full text-lg font-semibold">
-        {new Date(weatherData.LocalObservationDateTime).toLocaleDateString(
-          'id-ID',
-          {
-            weekday: 'long', // Display full weekday name (e.g., Senin)
-            year: 'numeric',
-            month: 'long', // Display full month name (e.g., Januari)
-            day: 'numeric',
-          },
-        )}
+      <p id="date" className="w-full text-lg font-medium">
+        {DateFormatter(weatherData.LocalObservationDateTime)}
       </p>
-      <p id="info" className="flex-1 text-lg font-normal">
+      <p id="info" className="w-full text-lg font-normal">
         {weatherData.WeatherText}
       </p>
-      <p id="temp" className="flex-1 text-2xl font-bold">
+      <p id="temp" className="w-full text-right text-xl font-bold">
         {weatherData.Temperature.Metric.Value}°C
-      </p>
-      <p id="temp" className="flex text-2xl font-bold">
-        {weatherData.WeatherIcon}
       </p>
     </div>
   );
+}
+
+function DateFormatter(date){
+  const formattedDate = new Date(date).toLocaleDateString(
+    'id-ID',
+    {
+      weekday: 'long', // Display full weekday name (e.g., Senin)
+      year: 'numeric',
+      month: 'long', // Display full month name (e.g., Januari)
+      day: 'numeric',
+    },
+  );
+
+  return formattedDate;
 }
