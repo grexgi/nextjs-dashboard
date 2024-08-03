4258f19b4596f814c69874eb5c82c34da2d0fcab
Grexgi <rfachri.exe@gmail.com>
2024-08-04 02:53:10 +0700

    try fetch from db

---
+++ app/test/page.tsx
@@ -0,0 +1,23 @@
+import supabase from "@/utils/supabase";
+
+export default async function Polybags() {
+    const { data, error } = await supabase.from("soil_sensor_monitoring")
+        .select()
+        .eq('polybag', 'B2-N2P1(3)')
+        .gte('created_at', '2024-08-04')
+        .lt('created_at', '2024-08-05')
+        .order('entry', { ascending: false });
+
+    if (error) {
+        console.error(error);
+        return <div>Error fetching data</div>;
+    }
+
+    if (!data || data.length === 0) {
+        return <div>No data found</div>;
+    }
+
+    console.log(data.length); // Optional chaining
+
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
---
+++ utils/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default createClient(
+    'https://qzqpdxrveobbvqliyxgw.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6cXBkeHJ2ZW9iYnZxbGl5eGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NzE5ODEsImV4cCI6MjAzNTA0Nzk4MX0.ihQsRCoXUKxvMO7qx0Nx0tUJ0EfJpqw2oJKzF_I68Q8'
+);
